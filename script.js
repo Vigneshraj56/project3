acae34e35e8c3181cc55c7f5e0e3ad9a1ee0c93f
Vigneshraj56 <vigneshraj702@gmail.com>
2025-02-25 18:52:05 +0530

    pro1

---
+++ script.js
@@ -0,0 +1,6 @@
+// Form validation or interactivity can be added here if needed
+
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Your message has been sent!");
+});
